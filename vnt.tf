eabb94d55d9d703a78b07a45cb1dc86027259131
rajubandi <rajubandi@gmail.com>
2026-03-28 12:10:19 +0530

    subnet added in Vnt

--- vnt.tf
+++ vnt.tf
@@ -4,3 +4,10 @@ resource "azurerm_virtual_network" "bandi" {
     location            = azurerm_resource_group.malli.location 
     address_space       = ["10.0.0.0/16"]
 }
+
+resource "azurerm_subnet" "sub" {
+  name = "subnet1"
+    resource_group_name  = azurerm_resource_group.malli.name
+    virtual_network_name = azurerm_virtual_network.bandi.name
+    address_prefixes     = ["10.0.1.0/24"]
+}
